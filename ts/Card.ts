e9eb90d7821a229914af046a847355a903d5d7a0
alvinnyen <alvinyen219@gmail.com>
2018-09-08 00:38:36 +0800

    add getRankName

--- ts/Card.ts
+++ ts/Card.ts
@@ -7,10 +7,30 @@ enum Suit {
 
 class Card {
     public readonly rank: number;
-    public readonly suit: number; // why number??
+    public readonly suit: number; // why number ??
 
     public constructor (rank: number, suit: Suit) {
         this.rank = rank;
         this.suit = suit;
+    }
+
+    private static rankNames = [ // why static ??
+        'Ace',
+        '2',
+        '3',
+        '4',
+        '5',
+        '6',
+        '7',
+        '8',
+        '9',
+        '10',
+        'Jack',
+        'Queen',
+        'King'
+    ];
+
+    public getRankName (): string {
+        return Card.rankNames[this.rank - 1];
     }
 }
