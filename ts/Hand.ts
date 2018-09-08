54f1afbf911314be596ee11350f6bbbc0c212c85
alvinnyen <alvinyen219@gmail.com>
2018-09-08 23:19:38 +0800

    add HandRankings

--- ts/Hand.ts
+++ ts/Hand.ts
@@ -7,3 +7,48 @@ interface Score {
     rank: HandRank,
     scoringCards: Card[],
 }
+
+let HandRankings: {
+    [x: string]: HandRank, // check object property type declaration in ts document
+} = {
+    ROYAL_FLUSH: {
+        name: 'Royal Flush',
+        payout: 800,
+    },
+    STRAIGHT_FLUSH: {
+        name: 'Straight Flush',
+        payout: 50,
+    },
+    FOUR_OF_A_KIND: {
+        name: 'Four of a Kind',
+        payout: 25,
+    },
+    FULL_HOUSE: {
+        name: 'Full House',
+        payout: 9,
+    },
+    FLUSH: {
+        name: 'Flush',
+        payout: 6,
+    },
+    STRAIGHT: {
+        name: 'Straight',
+        payout: 4,
+    },
+    THREE_OF_A_KIND: {
+        name: 'Three of a Kind',
+        payout: 3,
+    },
+    TWO_PAIR: {
+        name: 'Two Pair',
+        payout: 2,
+    },
+    JACKS_OR_BETTER: {
+        name: 'Jacks or Better',
+        payout: 1,
+    },
+    NOTHING: {
+        name: 'Nothing',
+        payout: 0,
+    },
+};
